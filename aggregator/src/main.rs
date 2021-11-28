b0a8c36bb348cbbcb2ee23c00feec52f4e6284da
fredrik <freddahf@outlook.com>
2021-11-28 13:37:30 +0100

    possible better way of joining the futures

--- aggregator/src/main.rs
+++ aggregator/src/main.rs
@@ -1,10 +1,11 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder};
+use futures::future::try_join_all;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::path::PathBuf;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Collector {
     room: String,
     url: String,
@@ -18,15 +19,15 @@ impl Collector {
 }
 
 #[derive(Serialize)]
-struct Temperature {
+struct EnvData {
     room: String,
     temperature: f64,
     humidity: f64,
 }
 
-impl Temperature {
+impl EnvData {
     fn new(room: String, temperature: f64, humidity: f64) -> Self {
-        Temperature {
+        EnvData {
             room,
             temperature,
             humidity,
@@ -34,27 +35,36 @@ impl Temperature {
     }
 }
 
+async fn get_temperature(client: web::Data<Client>, collector: &Collector) -> EnvData {
+    let resp: serde_json::Value = client
+        .get(&collector.url)
+        .send()
+        .await
+        .unwrap()
+        .json()
+        .await
+        .unwrap();
+    EnvData::new(
+        collector.room.clone(),
+        resp["temperature"].as_f64().unwrap(),
+        resp["humidity"].as_f64().unwrap(),
+    )
+}
+
 #[get("/")]
-async fn get_temp(
+async fn collect(
     client: web::Data<Client>,
-    stations: web::Data<Vec<Collector>>,
+    collectors: web::Data<Vec<Collector>>,
 ) -> impl Responder {
-    let mut res: Vec<Temperature> = Vec::new();
-    for station in stations.iter() {
-        let resp: serde_json::Value = client
-            .get(&station.url)
-            .send()
-            .await
-            .unwrap()
-            .json()
-            .await
-            .unwrap();
-        res.push(Temperature::new(
-            station.room.clone(),
-            resp["temperature"].as_f64().unwrap(),
-            resp["humidity"].as_f64().unwrap(),
-        ))
+    let mut responses: Vec<_> = Vec::new();
+    for collector in collectors.to_vec().iter() {
+        let tmp_collector = collector.clone();
+        let tmp_client = client.clone();
+        responses.push(tokio::spawn(async move {
+            get_temperature(tmp_client, &tmp_collector).await
+        }));
     }
+    let res = try_join_all(responses).await.unwrap();
     web::Json(res)
 }
 
@@ -62,7 +72,7 @@ async fn get_temp(
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(get_temp)
+            .service(collect)
             .app_data(web::Data::new(Client::new()))
             .app_data(web::Data::new(Collector::from_json(&PathBuf::from(
                 "collectors.json".to_string(),
