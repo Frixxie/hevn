790e203e06034ac777ab2615854ba261a0b75d0b
fredrik <freddahf@outlook.com>
2021-11-29 08:30:27 +0100

    logger should work

--- lumberjack/src/main.rs
+++ lumberjack/src/main.rs
@@ -1,7 +1,19 @@
+extern crate util;
 use reqwest::Client;
-
+use util::EnvData;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let client = Client::new();
+
+    let res: EnvData = client
+        .get("https://fasteraune.com/hevn")
+        .send()
+        .await
+        .unwrap()
+        .json()
+        .await
+        .unwrap();
+
+    println!("{}", res);
 }
