198f98da59f3b0a1d6e1358392e3b0f4fa892f78
fredrik <freddahf@outlook.com>
2022-01-05 19:01:21 +0100

    Closes #1 Added structopt to lumberjack project so we can have dynamic urls

--- lumberjack/src/main.rs
+++ lumberjack/src/main.rs
@@ -1,17 +1,26 @@
 extern crate util;
 use reqwest::Client;
+use structopt::StructOpt;
 use util::EnvData;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "Lumberjack", about = "Tool to interact with the Hevn project")]
+struct Opt {
+    #[structopt(
+        short = "u",
+        long = "url",
+        default_value = "https://fasteraune.com/hevn"
+    )]
+    url: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
 
-    let res: Vec<EnvData> = client
-        .get("https://fasteraune.com/hevn")
-        .send()
-        .await?
-        .json()
-        .await?;
+    let opt = Opt::from_args();
+
+    let res: Vec<EnvData> = client.get(opt.url.as_str()).send().await?.json().await?;
 
     for r in res {
         println!("{}", r);
