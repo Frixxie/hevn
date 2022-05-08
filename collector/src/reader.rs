7cba21b90fd4878c32d3e45931837f64892d37f3
fredrik <freddahf@outlook.com>
2022-05-08 18:32:53 +0200

    should help with the timeout error

--- collector/src/reader.rs
+++ collector/src/reader.rs
@@ -1,6 +1,8 @@
 use dht11::Dht11;
 use rppal::gpio::Gpio;
 use rppal::hal::Delay;
+use std::thread::sleep;
+use std::time::Duration;
 
 pub fn read_dht11(pin: u8) -> Result<(i16, u16), Box<dyn std::error::Error>> {
     let my_pin = Gpio::new()?.get(pin)?.into_io(rppal::gpio::Mode::Output);
@@ -15,6 +17,7 @@ pub fn read_dht11(pin: u8) -> Result<(i16, u16), Box<dyn std::error::Error>> {
             }
             Err(err) => {
                 println!("Error reading retrying.., {:?}", err);
+                sleep(Duration::from_millis(1000));
             }
         }
     }
